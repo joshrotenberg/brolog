0e2e621893e3e157a1984af2dadfb64c14ca8ecd
josh rotenberg <joshrotenberg@gmail.com>
2012-01-15 21:58:45 -0800

    update project for 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject brolog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject brolog "0.0.1"
+  :description "JNA Access to syslog(3) in Clojure"
   :dependencies [[clojure "1.3.0"]
-                 [net.java.dev.jna/jna "3.4.0"]])
+                 [net.java.dev.jna/jna "3.4.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
