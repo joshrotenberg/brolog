207bf7fae8b93833914237e8c7d5dcb51301f17d
Phil Crosby <phil.crosby@gmail.com>
2013-06-15 13:58:39 -0700

    Update project.clj dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject brolog "0.0.1"
   :description "JNA Access to syslog(3) in Clojure"
-  :dependencies [[clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.java.dev.jna/jna "3.4.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.9.1"]])
