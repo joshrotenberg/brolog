09d9f9b3cd10cb27f2250062d75ebb1cf911be4e
Phil Crosby <phil.crosby@gmail.com>
2013-06-15 14:47:18 -0700

    Add project URL

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject brolog "0.0.1"
+  :url "https://github.com/joshrotenberg/brolog"
   :description "JNA Access to syslog(3) in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.java.dev.jna/jna "3.4.0"]]
